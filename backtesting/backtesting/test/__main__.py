8859df532a25e8d5a9de2af65996919ffa0cd67d
zuongthao <zuongthao@gmail.com>
2020-10-22 11:19:45 +0700

    add submodule

--- backtesting/backtesting/test/__main__.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-suite = unittest.defaultTestLoader.discover('backtesting.test',
-                                            pattern='_test*.py')
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite)
