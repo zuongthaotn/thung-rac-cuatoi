8e7e8719f7b1c8d783e9bb7c17cd5747081d1016
zuongthao <zuongthao@gmail.com>
2020-12-03 13:58:09 +0700

    add RSI & SMA

--- algofuncs.py
+++ algofuncs.py
@@ -3,6 +3,20 @@ import pandas as pd
 import numpy as np
 from datetime import datetime
 
+def SMA(array, n):
+    """Simple moving average"""
+    return pd.Series(array).rolling(n).mean()
+
+
+def RSI(array, n):
+    """Relative strength index"""
+    # Approximate; good enough
+    gain = pd.Series(array).diff()
+    loss = gain.copy()
+    gain[gain < 0] = 0
+    loss[loss > 0] = 0
+    rs = gain.ewm(n).mean() / loss.abs().ewm(n).mean()
+    return 100 - 100 / (1 + rs)
 
 def MMI(data, period=100):
     '''
