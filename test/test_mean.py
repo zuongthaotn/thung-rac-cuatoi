3d9491bcf3e76e559fd2818e0097aed0b123500d
zuongthao <thao.duong@snaptec.io>
2022-06-26 22:22:38 +0700

    dev version 3

--- test/test_mean.py
+++ test/test_mean.py
@@ -1,9 +1,18 @@
-import scipy.stats as stats
-import numpy as np
+# Download the helper library from https://www.twilio.com/docs/python/install
+import os
+from twilio.rest import Client
 
-# We'll use these two data sets as examples
-x1 = [1, 2, 2, 3, 4, 5, 5, 7]
-x2 = x1 + [100]
-print(x2)
-print('Mean of x1:', sum(x1), '/', len(x1), '=', np.mean(x1))
-print('Mean of x2:', sum(x2), '/', len(x2), '=', np.mean(x2))
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+# account_sid = os.environ['AC6919b825005c39b1c46d590aeef7b510']
+# auth_token = os.environ['9db5cf5f51f00df00a58e534109c6da1']
+client = Client('AC6919b825005c39b1c46d590aeef7b510', '9db5cf5f51f00df00a58e534109c6da1')
+
+message = client.messages.create(
+                              body='Hi there, Fiverr here!',
+                              from_='+18055153327',
+                              to='+8801770001211'
+                          )
+
+print(message.sid)
