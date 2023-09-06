4410d64e9157e4c5690e2331861aa80cc05222ba
ZuongThao <thao@damda.com>
2023-09-06 10:06:46 +0700

    MACD v1

---
+++ MACD/v1.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import numpy as np
+import pandas_ta as ta
+from datetime import datetime
+
+def prepareData(htd):
+    if 'Time' in htd.columns:
+        htd['DateStr'] = htd.apply(
+            lambda x: datetime.fromtimestamp(x['Time']).strftime("%Y-%m-%d"), axis=1)
+    macd = ta.macd(htd['Close'], 20, 5, 9)
+    htd['Date'] = pd.to_datetime(htd['DateStr'])
+    htd = htd.assign(MACD=macd['MACD_5_20_9'])
+    htd = htd.assign(MACDh=macd['MACDh_5_20_9'])
+    htd = htd.assign(MACDs=macd['MACDs_5_20_9'])
+    ticker_data = htd.set_index('Date').drop(columns=['Time', 'DateStr'])
+    ticker_data['MACD'] = ticker_data['MACD'].replace(np.nan, 0)
+    ticker_data['MACDh'] = ticker_data['MACDh'].replace(np.nan, 0)
+    ticker_data['MACDs'] = ticker_data['MACDs'].replace(np.nan, 0)
+    return ticker_data
+
+def hasBuySignal(macd, signal):
+    """
+
+    """
+    today_macd = macd[-1]
+    today_signal = signal[-1]
+    yesterday_macd = macd[-2]
+    yesterday_signal = signal[-2]
+    print("----{}------{}----{}------{}", today_macd, today_signal, yesterday_macd, yesterday_signal)
+    if yesterday_macd < yesterday_signal and today_macd > today_signal:
+        return True
+    return False
+
+def hasSellSignal(macd, signal):
+    """
+
+    """
+    today_macd = macd[-1]
+    today_signal = signal[-1]
+    yesterday_macd = macd[-2]
+    yesterday_signal = signal[-2]
+    if yesterday_macd > yesterday_signal and today_macd < today_signal:
+        return True
+    return False
