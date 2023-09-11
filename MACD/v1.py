63f998c1cdd4dccc891a43919cc2458ef05a5479
zuongthao <thao.duong@snaptec.io>
2023-09-11 22:38:41 +0700

    AI neat MACD

--- MACD/v1.py
+++ MACD/v1.py
@@ -4,11 +4,13 @@ import pandas_ta as ta
 from datetime import datetime
 
 CUTLOSS_PERCENT = 0.97
+TAKE_PROFIT_PERCENT = 1.1
 
 def prepareData(htd):
     if 'Time' in htd.columns:
         htd['DateStr'] = htd.apply(
             lambda x: datetime.fromtimestamp(x['Time']).strftime("%Y-%m-%d"), axis=1)
+    htd['MA_5'] = htd['Close'].rolling(window=5).mean()
     macd = ta.macd(htd['Close'], 20, 5, 9)
     htd['Date'] = pd.to_datetime(htd['DateStr'])
     htd = htd.assign(MACD=macd['MACD_5_20_9'])
@@ -48,5 +50,17 @@ def hasSellSignal(macd, signal):
 
 def has_force_sell_signal(current_price, buy_price):
     if current_price < buy_price * CUTLOSS_PERCENT:
+        return True
+    return False
+
+def take_profit(current_price, buy_price):
+    if current_price > buy_price * TAKE_PROFIT_PERCENT:
+        return True
+    return False
+
+def hasSellSignal02(today_open, today_close, peak_price, today_ma5):
+    if today_close > today_ma5:
+        return False
+    if (today_close < today_ma5 and today_open - today_close > 0 and today_close/peak_price > 0.04):
         return True
     return False
